a5fc3a9aa0b279eef4921c8d1f2cf75eeecd4f84
napiorkowskik83 <napiorkowskik83@gmail.com>
2021-02-22 23:54:46 +0100

    First Commit

---
+++ src/test/java/com/task/genderdetector/GenderDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.genderdetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenderDetectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
